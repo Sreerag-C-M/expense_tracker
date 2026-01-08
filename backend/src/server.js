8b5f6c369a4066fa29a31ed15ad0e56c74527518
Sreerag-C-M <sreeragcm12@gmail.com>
2026-01-08 22:24:35 +0530

    feat: add root route to prevent 404 error

--- backend/src/server.js
+++ backend/src/server.js
@@ -15,6 +15,10 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+app.get('/', (req, res) => {
+    res.send('Expense Tracker API is running. Go to /graphql to use the API.');
+});
+
 app.use('/graphql', graphqlHTTP({
     schema,
     graphiql: true
